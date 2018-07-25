be86e0a0123f419c5b0321bb3e7e6cef4a2e8c7d
yoheimuta <yoheimuta@gmail.com>
2018-07-25 11:34:47 +0900

    init

---
+++ BufferedLogger/BufferedLogger.h
@@ -0,0 +1,19 @@
+//
+//  BufferedLogger.h
+//  BufferedLogger
+//
+//  Created by YOSHIMUTA YOHEI on 2018/07/25.
+//  Copyright © 2018年 YOSHIMUTA YOHEI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BufferedLogger.
+FOUNDATION_EXPORT double BufferedLoggerVersionNumber;
+
+//! Project version string for BufferedLogger.
+FOUNDATION_EXPORT const unsigned char BufferedLoggerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BufferedLogger/PublicHeader.h>
+
+
